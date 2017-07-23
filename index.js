f2f84f472b4b13a9915615373d38d8c2fdf40ac4
Artico Bandurini <artico.bandurini@gmail.com>
2017-07-23 12:09:48 +0200

    support for strict and replace parameters, pass-thru target headers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const	http = require('http'),
 		request = require('request'),
 		url = require('url'),
@@ -8,53 +10,51 @@ module.exports = class Jsoxy {
 	constructor(options) {
 		
 		if (!options.target) throw new Error('Target URL is required for Jsoxy to start!');
-		
-		if (options.editors) this.editors = options.editors;
+		this.strict = !!options.strict;
+		this.editors = options.editors || {};
 		
 		return http.createServer((req, res) => {
-
-			const send = this.sender(req, res);
-			const method = req.method.toLowerCase();
-	
-			switch (method) {
-				case 'get':
-					request(options.target + url.parse(req.url).path, (err, proxy, input) => {								
-						if (err) {
-							send(err, null, 502);
-						}
-						else {
-							try {
-								this.responder(req, res)(method, input, send);
-							}
-							catch (err) {
-								send(err);
-							}
-						}
-					});
-				break;
-				default:
-					send(null, null, 405);
-				break;
-			}
-	
+			res.url = options.target + url.parse(req.url).path;
+			this.responder(req, res)(this.sender(req, res));
 		});
 	};
 	
 
 	responder(req, res) {
 		
-		return (method, input, send) => {
-			const body = JSON.parse(input);
-			let promise = Promise.resolve(body);
+		return (send) => {
+
+			const method = req.method.toLowerCase();
+			let editor = null;
+			
 			for (let i in this.editors) {
 				if (url.parse(req.url).pathname.match(this.editors[i].match) && typeof this.editors[i][method] === 'function') {
-					promise = this.editors[i][method](body);
+					editor = this.editors[i][method];
+					if (this.editors[i].replace) res.url = res.url.replace(this.editors[i].match, this.editors[i].replace);
 					break;
 				}
 			}
-			promise
-				.then(body => send(null, JSON.stringify(body)))
-				.catch(err => send(err));
+			if (editor || !this.strict) {
+				request(res.url, (err, proxy, input) => {
+					if (err) {
+						send(err, null, 502);
+					}
+					else {
+						try {
+							const body = JSON.parse(input);
+							(editor ? editor(body) : Promise.resolve(body))
+								.then(body => send(null, JSON.stringify(body), proxy.statusCode, proxy.statusMessage, proxy.headers))
+								.catch(err => send(err));
+						}
+						catch (err) {
+							send(err);
+						}
+					}
+				});
+			}
+			else {
+				send(null, null, 403);
+			}
 		};
 	};
 	
@@ -63,17 +63,22 @@ module.exports = class Jsoxy {
 		
 		return (err, data, code, message, headers) => {
 	
-			const _send = (err, data, code, message, headers) => {
+			const _send = (err, data, code, message, headers, encoding) => {
 				if (!data) data = '';
-				res.setHeader('Content-Length', Buffer.byteLength(data, 'utf-8'));
-				res.setHeader('Content-Type', 'application/json; charset=utf-8');
-				res.setHeader('Access-Control-Allow-Origin', '*');
-				res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-				res.setHeader('Expires', 'Thu, 01 Jan 1970 00:00:00 GMT');
-				res.setHeader('X-Powered-By', 'Jsoxy');
+				if (!headers) headers = {};
+				if (encoding) headers['content-encoding'] = encoding;
+				headers['content-length'] = Buffer.byteLength(data, 'utf-8');
+				headers['x-powered-by'] = 'Jsoxy';
+				
+				for (let i in headers) {
+					let c = i.replace(/\b\w/g, i => i.toUpperCase());
+					headers[c] = headers[i];
+					if (c !== i) delete headers[i];
+				}
+				
 				res.writeHead(code || (err ? 500 : 200), message || '', headers);
 				res.end(data, 'utf-8', () => {
-					console.log(new Date, req.url);
+					console.log(new Date + ': ' + req.method + ' ' + req.url + '\n > ' + res.url);
 					if (err) console.log('Server Error:', err);
 				});
 			};
@@ -84,8 +89,7 @@ module.exports = class Jsoxy {
 				for (let encoding of ['deflate', 'gzip']) {
 					if (encodings.includes(encoding)) {
 						zlib[encoding](new Buffer(data || '', 'utf-8'), (e, data) => {
-							res.setHeader('Content-Encoding', encoding);
-							_send(e || err, data, code, message, headers);
+							_send(e || err, data, code, message, headers, encoding);
 						});
 						encoded = true;
 						break;
