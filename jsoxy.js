0f25e666e2d870d3b2de0f999112323e5ac14ad7
Artico <artico.bandurini@gmail.com>
2020-11-08 08:27:10 +0200

    Refactoring of jsoxy.js

--- jsoxy.js
+++ jsoxy.js
@@ -3,33 +3,25 @@
 const	Jsoxy = require('./index.js'),
 	fs = require('fs');
 
-var options = { editors: {} };
-
-const path = process.cwd() + '/.jsoxy';
+const options = { editors: {} },
+	path = process.cwd() + '/.jsoxy';
 if (fs.existsSync(path)) {
-	const _read = path => {
-		try {
-			return JSON.parse(fs.readFileSync(path, 'utf8'));
-		}
-		catch (err) {
-			console.error(err);
-			return null;
-		}
-	};
-	
-	const optFile = path + '/jsoxy.json';
-	options = fs.existsSync(optFile) ? _read(optFile) : {};
-	
+	try { options = require(path + '/jsoxy.json'); } catch (e) {}
 	if (!options.editors) options.editors = {};
 	const files = fs.readdirSync(path, { encoding: 'utf8' });
-	files.forEach(file => {
-		if (file.match(/\.js$/)) options.editors[file.replace(/\.js$/, '')] = require(path + '/' + file);
+	files.forEach((file, i) => {
+		if (i = file.match(/(.*)\.js$/i)) {
+			options.editors[i] = require(path + '/' + file);
+		}
 	});
 }
 
 options.target = process.argv[2] || options.target || 'https://api.github.com';
 options.port = process.argv[3] || options.port || 8008;
+options.strict = process.argv[4] === 'true' || options.strict || false;
+console.log(options);
+(new Jsoxy(options)).listen(options.port);
 
-const jsoxy = new Jsoxy(options);
-jsoxy.listen(options.port);
-console.log('Jsoxy started on port ' + options.port + ' [' + options.target + '] with ' + Object.keys(options.editors).length + ' editor(s).');
+console.log('Jsoxy started on port ' + options.port
+ + ' [' + options.target + '] with '
+ + Object.keys(options.editors).length + ' editor(s).');
