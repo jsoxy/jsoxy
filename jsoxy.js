aa0a91331405e15561884331f10251fafa2395fa
Artico <artico.bandurini@gmail.com>
2020-11-12 11:31:20 +0200

    add Dockerfile

--- jsoxy.js
+++ jsoxy.js
@@ -19,7 +19,7 @@ if (fs.existsSync(path)) {
 options.target = process.argv[2] || options.target || 'https://api.github.com';
 options.port = process.argv[3] || options.port || 8008;
 options.strict = process.argv[4] === 'true' || options.strict || false;
-console.log(options);
+
 (new Jsoxy(options)).listen(options.port);
 
 console.log('Jsoxy started on port ' + options.port
